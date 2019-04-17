58a74d778254b3cf342386c6fd860f96e3813fba
Oliver <oliverskoeppen@gmail.com>
2019-04-17 08:12:27 +0200

    fixed brand search

---
+++ src/app/shared/search/brand-search.builder.ts
@@ -0,0 +1,21 @@
+export class BrandSearchBuilder implements ISearch {
+  query: string;
+  private contains: string = '';
+  private orderBy: string = '';
+
+
+  setContains(keyToContain: string, name: string) {
+    this.orderBy = `$Filter=${keyToContain} eq '${name}'`;
+    return this;
+  }
+  setOrderBy(name: string) {
+    this.contains = `$OrderBy=${name}&`;
+    return this;
+  }
+
+  build() {
+    this.query = `brands?${this.contains}${this.orderBy}`;
+    return this;
+  }
+
+}
