3ee95a76f858f627de8e5dd44a8fcf84caaf2f01
Oliver <oliverskoeppen@gmail.com>
2019-04-18 10:03:08 +0200

    Added paginator

--- src/app/brands/brands.component.ts
+++ src/app/brands/brands.component.ts
@@ -15,12 +15,17 @@ export class BrandsComponent implements OnInit {
   searchComplete: boolean = false;
   brandSearchBuilder: BrandSearchBuilder;
   protected toShow: number = 10;
+  protected currentPage: number = 1;
+  protected pageList: number[];
 
-  optionsToShow = [
+  previousButton: boolean = false;
+  nextButton: boolean = false;
+
+ optionsToShow = [
     {name : "10", value: 10},
     {name : "25", value: 25},
     {name : "50", value: 50},
-  ]
+  ];
 
   constructor(private webService: WebService) { }
 
@@ -41,43 +46,77 @@ export class BrandsComponent implements OnInit {
     console.log(this.brandSearchBuilder.query);
     this.webService.getBrandsByQuery(this.brandSearchBuilder.query)
       .subscribe((data: Brand[]) => {
-          console.log(data);
           this.brandList = data.map(
             (brand) => {
               return new Brand(brand.brandID, brand.brandName);
             }
           );
           this.searchComplete = true;
+          this.initPages();
         },
         (error: Data) => {
           console.log(error);
         });
   }
 
-  private getAllBrands() {
-    this.webService.getBrands()
-      .subscribe((data: Brand[]) => {
-          console.log(data);
-          this.brandList = data.map(
-            (brand) => {
-              return new Brand(brand.brandID, brand.brandName);
-            }
-          );
-          this.searchComplete = true;
-        },
-        (error: Data) => {
-          console.log(error);
-        });
+  showPagesFrom() {
+    return this.toShow*(this.currentPage-1);
   }
 
-  setSort(toSort: number) {
-    switch (toSort) {
-      case 0: this.brandSearchBuilder.setOrderBy('brandName'); break;
-      case 1: this.brandSearchBuilder.setOrderBy('id'); break;
+  showPagesTo() {
+    return this.toShow*(this.currentPage);
+  }
+
+  initPages () {
+    if (this.brandList == null ) {
+      this.pageList = new Array(0)
+    }
+    else {
+      this.pageList = this.fillArrayWithNumbers(Math.ceil(this.brandList.length / this.toShow));
+      console.log(this.pageList);
+      this.checkForButtonChanges(this.pageList[0]);
     }
   }
 
-  setShowMax(value: string) {
-    this.toShow = +value;
+  setCurrentPage(chosenPage: number) {
+
+    if (this.pageList.length > 5) {
+      if ((this.pageList.length - chosenPage) >= 2 || chosenPage <= 2) {
+        for (let i of this.pageList) {
+          this.pageList[i] = this.pageList[i] +1;
+        }
+      }
+      else {
+        this.currentPage = chosenPage;
+      }
+    }
+      this.currentPage = chosenPage;
+
+    this.checkForButtonChanges(chosenPage);
+  }
+
+  increasePage() {
+    this.setCurrentPage(this.currentPage+1);
+  }
+
+  decreasePage() {
+    this.setCurrentPage(this.currentPage-1);
+  }
+
+  fillArrayWithNumbers(length: number) {
+    var arr = Array.apply(null, Array(length));
+    return arr.map( function (x,i) {
+        return i;
+    })
+  }
+
+  private checkForButtonChanges(chosenPage: number) {
+    (chosenPage > 1)? this.previousButton = true : this.previousButton = false;
+    (chosenPage < this.pageList.length)? this.nextButton=true : this.nextButton = false;
+    // if (chosenPage < this.pageList.length) {
+    //   this.nextButton = true;
+    // } else if (chosenPage >= this.pageList.length) {
+    //   this.nextButton = false;
+    // }
   }
 }
