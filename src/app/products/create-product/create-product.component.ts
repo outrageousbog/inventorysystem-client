5aaac0c0ab537e98de7456375f4b2882acb24cb5
Oliver <oliverskoeppen@gmail.com>
2019-05-02 17:17:02 +0200

    multiple materials now possible

--- src/app/products/create-product/create-product.component.ts
+++ src/app/products/create-product/create-product.component.ts
@@ -16,19 +16,22 @@ export class CreateProductComponent implements OnInit {
   materialList: Material[];
   n=1;
 
-  constructor(private createService: CreateProductService) { }
+  constructor(private createService: CreateProductService,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.productForm = new FormGroup({
       productSKU: new FormControl(12345678, [Validators.required]),
       productName: new FormControl('Eris E5',[Validators.required]),
       productBrand: new FormControl('Presonus',[Validators.required]),
-      productMaterialsID: new FormControl(4,[Validators.required]),
       productPrice: new FormControl(2000,[Validators.required]),
       productVariableCost: new FormControl(1500,[Validators.required]),
       productStartFactor: new FormControl(400,[Validators.required]),
       productGrowthFactor: new FormControl(20,[Validators.required]),
+      productMaterialsID: this.formBuilder.array([])
     });
+
+
 
     this.createService.materialSubject
       .subscribe(
@@ -54,5 +57,25 @@ export class CreateProductComponent implements OnInit {
   onSubmit() {
     console.log(this.productForm.value);
     //this.createService.createProduct(this.productForm.value);
+  }
+
+  /**
+   * Med hjælp og inspiration fra:
+   * https://angularfirebase.com/lessons/basics-reactive-forms-in-angular/
+   */
+  get materialForms() {
+    return this.productForm.controls.productMaterialsID as FormArray;
+  }
+
+  addMaterial() {
+    const materialArray = this.formBuilder.group({
+      materialName: [null, [Validators.required]]
+    });
+
+    this.materialForms.push(materialArray);
+  }
+
+  removeMaterial(index: number) {
+    this.materialForms.removeAt(index);
   }
 }
